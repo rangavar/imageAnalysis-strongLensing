c8f445dfa1073d74a7c001eb5f349e59ea708e86
Danial Rangavar Langeroodi <dlangaroudi@gmail.com>
2020-10-20 01:52:17 +0200

    visual selection

--- png.py
+++ png.py
@@ -4,17 +4,41 @@ import numpy as np
 import os
 import sys
 import pyplz_rgbtools
-
-
-# Marshall, P. et al. (2016) "Space Warps I"
+import h5py
 
 f = open('0simulations/filter/sim0_lens.cat', 'r')
 lines = f.readlines()
 f.close()
+lines = lines[1:]
+
+data = h5py.File('0simulations/filter/sim0_parameters.hdf5', 'r')
+i_mags = {}
+count = 0
+for line in lines:
+    ID = line.split(' ')[0]
+    i_mags[ID] = data['source_mag_i'][count]
+    count = count + 1
+
+temp_sorted_list = sorted(i_mags, key=i_mags.get)
+delete_list = [0,15,21,26,27,28,29,30,31,32,41,42,43,45,47,48,49,53,54,55,59,61,64,66]
+#delete_list = []
+delete_id = []
+for key in delete_list:
+    delete_id.append(temp_sorted_list[key])
+clean_sorted_list = []
+for key in temp_sorted_list:
+    if key not in delete_id:
+        clean_sorted_list.append(key)
+
+sorted_list = {}
+for name in clean_sorted_list:
+    sorted_list[name] = i_mags[name]
+print(len(sorted_list))
 
 ncol = 5
-nlens = len(lines)
+nlens = len(sorted_list)
 nrow = nlens/ncol
+nrow = int(nrow)
 if nlens%ncol > 0:
     nrow += 1
 
@@ -48,18 +72,19 @@ def make_rgb(rootname, rgbbands, datadir='/', outputdir='./'):
     return im
 
 npix = 101
-
 fullimg = Image.new('RGB', (ncol*npix, nrow*npix), 'black')
 
-for i in range(nlens):
+count = 0
+for name in sorted_list.keys():
+    print(name,sorted_list[name])
 
-    name = lines[i+24].split()[0]
-
-    colno = i%ncol
-    rowno = i/ncol
+    colno = int(count%ncol)
+    rowno = int(count/ncol)
 
     im = make_rgb(name, rgbbands, datadir=datadir)
 
     fullimg.paste(im, (npix*colno, npix*rowno))
 
-fullimg.save('0simulations/sample_collage_%dx%d.png'%(nrow, ncol))
+    count = count + 1
+
+fullimg.save('0simulations/sample_collage.png')
