12391b9637923d1e37e28649e46f00777423f4db
Danial Rangavar Langeroodi <dlangaroudi@gmail.com>
2020-10-21 03:35:39 +0200

    added the ratio plot
    plots the lens to source sb ratio now

--- plot.py
+++ plot.py
@@ -5,6 +5,7 @@ import numpy
 import matplotlib.pyplot as plt
 import glob
 import os
+from astropy.io import fits
 
 os.makedirs('41-1-011..020/progress/vol2outputs')
 os.makedirs('41-1-031..040/progress/vol2outputs')
@@ -95,7 +96,7 @@ for directory in directories:
 for name in names:
     sim_color = float(mags[name].split(' ')[1])-float(mags[name].split(' ')[5])
     real_color = real_data[name][0]-real_data[name][2]
-    color_error = abs(sim_color-real_color)
+    color_error = (sim_color-real_color)
     real_data[name].append(sim_color)
     real_data[name].append(real_color)
     real_data[name].append(color_error)
@@ -117,8 +118,8 @@ for name in names:
     for i in range(5):
         if float(mags[name].split(' ')[2+2*i])>1.0 or real_data[name][i]>26.0 or real_data[name][i]<20.0 or float(mags[name].split(' ')[1+2*i]) > 26.0 or float(mags[name].split(' ')[1+2*i]) < 20.0:
             error_names.append(name)
-    if real_data[name][-1]<0.5 or real_data[name][-1]>0.75 or real_data[name][-2]<0.5 or real_data[name][-2]>0.75:
-        real_data[name][-1]< 0.5
+    if real_data[name][-2]<-0.5 or real_data[name][-2]>0.75 or real_data[name][-3]<-0.5 or real_data[name][-3]>0.75 or real_data[name][-1]>0.5:
+        error_names.append(name)
 
 #deleting the extremely wrong objects
 error_names = numpy.unique(error_names)
@@ -173,8 +174,90 @@ y = numpy.arange(-30,30,1)
 fit = numpy.polyfit(x,y,1)
 fitFunction = numpy.poly1d(fit)
 
- 
+#plotting the real vs fit mags and colours
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size = '24',weight='bold')
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['ytick.right'] = plt.rcParams['ytick.labelright'] = False
+plt.rcParams['ytick.left'] = plt.rcParams['ytick.labelleft'] = True
+plt.rcParams['xtick.minor.visible'] = True
+plt.rcParams['ytick.minor.visible'] = True
+plt.figure(figsize=(14, 21))
+x_lim = 19.5
+y_lim = 26.5
+fontsize = 30
+marker = 'x'
+size = 12
+plt.rcParams["errorbar.capsize"] = 0.
+mfc = 'darkslategray'
+mec = 'darkslategray'
+color = 'olivedrab'
+ecolor = 'lightslategray'
+elinewidth = 0.75
+alpha=0.01
 
+#g-band
+plt.subplot(321)
+plt.plot(x,fitFunction(x),color=color, label = 'fit mag = true mag',alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realMagG,magG,yerr=Ger,marker=marker,linestyle="None",markerfacecolor=mfc,label='simulated mock-sample',markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlim(x_lim,y_lim)
+plt.ylim(x_lim,y_lim)
+plt.xlabel('$m_{g,true}$',fontsize=fontsize)
+plt.ylabel('$m_{g,fit}$',fontsize=fontsize)
+plt.legend(fontsize=22)
+
+#r_band
+plt.subplot(322)
+plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realMagR,magR,yerr=Rer,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlim(x_lim,y_lim)
+plt.ylim(x_lim,y_lim)
+plt.xlabel('$m_{r,true}$',fontsize=fontsize)
+plt.ylabel('$m_{r,fit}$',fontsize=fontsize)
+
+#i-band
+plt.subplot(323)
+plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realMagI,magI,yerr=Ier,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlim(x_lim,y_lim)
+plt.ylim(x_lim,y_lim)
+plt.xlabel('$m_{i,true}$',fontsize=fontsize)
+plt.ylabel('$m_{i,fit}$',fontsize=fontsize)
+
+#y-band
+plt.subplot(324)
+plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realMagZ,magZ,yerr=Zer,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlim(x_lim,y_lim)
+plt.ylim(x_lim,y_lim)
+plt.xlabel('$m_{z,true}$',fontsize=fontsize)
+plt.ylabel('$m_{z,fit}$',fontsize=fontsize)
+
+#z-band
+plt.subplot(325)
+plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realMagY,magY,yerr=Yer,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlim(x_lim,y_lim)
+plt.ylim(x_lim,y_lim)
+plt.xlabel('$m_{y,true}$',fontsize=fontsize)
+plt.ylabel('$m_{y,fit}$',fontsize=fontsize)
+
+#colours
+plt.subplot(326)
+plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=1) 
+plt.errorbar(realColor,simColor,yerr=Ier,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
+plt.xlabel('true $g$-$i$ colour',fontsize=fontsize-8)
+plt.ylabel('fit $g$-$i$ colour',fontsize=fontsize-8)
+plt.xlim(-0.45,0.80)
+plt.ylim(-0.45,0.80)
+plt.xticks(numpy.arange(-0.25, 1.0, 0.5))
+plt.yticks(numpy.arange(-0.25, 1.0, 0.25))
+plt.tight_layout()
+plt.savefig('paper_plots/all.eps',format='eps',dpi=1000)
+plt.close()
+
+#saving the data
 finalLines = []
 finalDataLines = []
 for i in range(len(realMagG)):
@@ -186,3 +269,86 @@ for i in range(len(realMagG)):
 f = open('paper_plots/output.txt','w')
 f.writelines(finalLines)
 f.close()
+
+f = open('paper_plots/data.txt','w')
+f.writelines(finalDataLines)
+f.close()
+
+################################
+#for the lens to source SB ratio
+os.makedirs('sources')
+
+lines = []
+lines.append('name ratio error\n')
+for name in mags.keys():
+    if name in names_address[directories[0]]: directory = directories[0]
+    if name in names_address[directories[1]]: directory = directories[1]
+    noSource = fits.open('../rangavar/13tousand/3quarry/data/%s_i_sci.fits'%name)
+    imageFile = fits.open('%s/data/data/sim0_%s_i_sci.fits'%(directory,name))
+    image = imageFile[0].data
+    noSource[0].data = image - noSource[0].data
+
+    #finding the source maximum pixel
+    xRange,yRange = numpy.shape(noSource[0].data)
+    iMax = 0.
+    for x_pixel in range(xRange):
+        for y_pixel in range(yRange):
+            if noSource[0].data[x_pixel][y_pixel] >= iMax:
+                iMax = noSource[0].data[x_pixel][y_pixel]
+                sourcex = x_pixel
+                sourcey = y_pixel
+    print(iMax)
+
+    #lens again
+    newnoSource = fits.open('../rangavar/13tousand/3quarry/data/%s_i_sci.fits'%name)
+    lens = float(newnoSource[0].data[sourcex,sourcey])
+    source = float(noSource[0].data[sourcex,sourcey])
+    noSource.writeto('sources/%s_source.fits'%name)
+    ratio = abs(float(lens/source))
+    real_data[name].append(ratio)
+    lines.append('%s %f %f\n'%(name,ratio,real_data[name][8]))
+    count = count + 1
+
+f = open('paper_plots/ratio.txt','w')
+f.writelines(lines)
+f.close()
+
+#plotters
+ratio = []
+error = []
+for name in mags.keys():
+    if abs(real_data[name][-2]) > 3e-4:
+        ratio.append(real_data[name][-1])
+        error.append(abs(real_data[name][-2]))
+
+#fit line
+fitRatio = numpy.log10(ratio)
+fitError = numpy.log10(error)
+p = numpy.polyfit(fitRatio, fitError, 1)
+fittedLine = numpy.poly1d(p)
+x = numpy.arange(min(fitRatio-2),max(fitRatio+2),0.1)
+y = fittedLine(x)
+
+#plotting
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size = '24',weight='bold')
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['ytick.right'] = plt.rcParams['ytick.labelright'] = False
+plt.rcParams['ytick.left'] = plt.rcParams['ytick.labelleft'] = True
+plt.rcParams['xtick.minor.visible'] = True
+plt.rcParams['ytick.minor.visible'] = True
+plt.rcParams['xtick.major.pad'] = 8
+plt.figure(figsize=(10, 8))
+
+plt.plot(10.**x, 10.**y, c='darkgoldenrod', linestyle='--', linewidth = 3, label='best-fit line',zorder=1)
+plt.scatter(ratio,error,c='midnightblue',s=40,marker='x',label='simulated mock-images',zorder=10)
+plt.xscale('log')
+plt.yscale('log')
+plt.ylim(3e-4,1)
+plt.xlim(1e-3,10)
+plt.legend(fontsize='22')
+plt.xlabel('lens to source SB ratio ($\Lambda_{i}$)',fontsize=26)
+plt.ylabel('$g$-$i$ error ($\delta C$)',fontsize=26)
+plt.savefig('paper_plots/ratio.eps',format='eps', dpi=1000)
+plt.close()
