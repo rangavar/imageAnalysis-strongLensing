4328eef1119f6580db77bf43b2f225624999dee3
Danial Rangavar Langeroodi <dlangaroudi@gmail.com>
2020-10-22 17:19:35 +0200

    wraps all the other scripts
    all the scripts in one place on multiple simulated samples

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import glob
 import os
 from astropy.io import fits
+import math
 
 os.makedirs('41-1-011..020/progress/vol2outputs')
 os.makedirs('41-1-031..040/progress/vol2outputs')
@@ -177,6 +178,8 @@ fitFunction = numpy.poly1d(fit)
 #plotting the real vs fit mags and colours
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size = '24',weight='bold')
+plt.rcParams["font.family"]='serif'
+plt.rcParams["font.serif"][0] = 'times'
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['ytick.right'] = plt.rcParams['ytick.labelright'] = False
@@ -186,13 +189,13 @@ plt.rcParams['ytick.minor.visible'] = True
 plt.figure(figsize=(14, 21))
 x_lim = 19.5
 y_lim = 26.5
-fontsize = 30
+fontsize = 40
 marker = 'x'
 size = 12
 plt.rcParams["errorbar.capsize"] = 0.
 mfc = 'darkslategray'
 mec = 'darkslategray'
-color = 'olivedrab'
+color = 'goldenrod' #'olivedrab'
 ecolor = 'lightslategray'
 elinewidth = 0.75
 alpha=0.01
@@ -249,14 +252,15 @@ plt.plot(x,fitFunction(x),color=color,alpha=alpha,linewidth=elinewidth+2,zorder=
 plt.errorbar(realColor,simColor,yerr=Ier,marker=marker,linestyle="None",markerfacecolor=mfc,markersize=size,mec=mec,ecolor=ecolor,elinewidth=elinewidth,zorder=10)
 plt.xlabel('true $g$-$i$ colour',fontsize=fontsize-8)
 plt.ylabel('fit $g$-$i$ colour',fontsize=fontsize-8)
-plt.xlim(-0.45,0.80)
-plt.ylim(-0.45,0.80)
+plt.xlim(-0.40,0.80)
+plt.ylim(-0.40,0.80)
 plt.xticks(numpy.arange(-0.25, 1.0, 0.5))
 plt.yticks(numpy.arange(-0.25, 1.0, 0.25))
 plt.tight_layout()
 plt.savefig('paper_plots/all.eps',format='eps',dpi=1000)
 plt.close()
 
+#Ger = Rer = Ier = Zer = Yer = numpy.zeros(len(Ger))+0.035
 #saving the data
 finalLines = []
 finalDataLines = []
@@ -307,7 +311,6 @@ for name in mags.keys():
     ratio = abs(float(lens/source))
     real_data[name].append(ratio)
     lines.append('%s %f %f\n'%(name,ratio,real_data[name][8]))
-    count = count + 1
 
 f = open('paper_plots/ratio.txt','w')
 f.writelines(lines)
@@ -316,10 +319,16 @@ f.close()
 #plotters
 ratio = []
 error = []
+error_names = []
 for name in mags.keys():
-    if abs(real_data[name][-2]) > 3e-4:
-        ratio.append(real_data[name][-1])
-        error.append(abs(real_data[name][-2]))
+    if abs(real_data[name][-2]) < 3e-4:
+        error_names.append(name)
+for name in error_names:
+    del(mags[name])
+    del(real_data[name])
+for name in mags.keys():
+    ratio.append(real_data[name][-1])
+    error.append(abs(real_data[name][-2]))
 
 #fit line
 fitRatio = numpy.log10(ratio)
@@ -331,7 +340,9 @@ y = fittedLine(x)
 
 #plotting
 plt.rc('text', usetex=True)
-plt.rc('font', family='serif', size = '24',weight='bold')
+plt.rc('font', family='serif', size = '22',weight='bold')
+plt.rcParams["font.family"]='serif'
+plt.rcParams["font.serif"][0] = 'times'
 plt.rcParams['xtick.direction'] = 'in'
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['ytick.right'] = plt.rcParams['ytick.labelright'] = False
@@ -347,8 +358,165 @@ plt.xscale('log')
 plt.yscale('log')
 plt.ylim(3e-4,1)
 plt.xlim(1e-3,10)
-plt.legend(fontsize='22')
-plt.xlabel('lens to source SB ratio ($\Lambda_{i}$)',fontsize=26)
-plt.ylabel('$g$-$i$ error ($\delta C$)',fontsize=26)
+plt.legend(fontsize='20')
+plt.xlabel('lens to source SB ratio ($\Lambda_{i}$)',fontsize=24)
+plt.ylabel('$g$-$i$ error ($\delta C$)',fontsize=24)
 plt.savefig('paper_plots/ratio.eps',format='eps', dpi=1000)
+plt.close()
+
+###############
+#lens at source 
+os.makedirs('lensResAtSource')
+os.makedirs('lensResAtSource/sources')
+os.makedirs('lensResAtSource/res')
+
+filters = ['g','r','i','z','y']
+newlines = []
+ratio = {}
+newlines.append('name filter resAtsourceSB sourceSB res/source I(Re)\n')
+for name in mags.keys():
+    if name in names_address[directories[0]]: directory = directories[0]
+    if name in names_address[directories[1]]: directory = directories[1]
+    f = open('%s/progress/vol2outputs/config%s'%(directory,name),'r')
+    lines = f.readlines()
+    f.close()
+
+    re = float(lines[38].split(' ')[1])
+    n = float(lines[40].split(' ')[1])
+    mag = {}
+    mag['g'] = lines[41].split(' ')[1].split('\n')[0]
+    mag['r'] = lines[42].split(' ')[1].split('\n')[0]
+    mag['i'] = lines[43].split(' ')[1].split('\n')[0]
+    mag['z'] = lines[44].split(' ')[1].split('\n')[0]
+    mag['y'] = lines[45].split(' ')[1].split('\n')[0]
+
+    #step0 = fits.open('progress/0outputs/config%s_ML.fits'%name)
+    sourcePath = glob.glob('%s/progress/vol2outputs/config%s_*'%(directory,name))
+    sourcePath = sourcePath[0].split('_')[-2]
+    source = fits.open('%s/progress/%soutputs/config%s_%s_ML.fits'%(directory,int(sourcePath)+1,name,sourcePath))
+    step0 = fits.open('%s/progress/%soutputs/config%s_%s_ML.fits'%(directory,int(sourcePath)+1,name,sourcePath))
+    count = 1
+    for f in filters:
+        #os.popen('cp data/data/sim0_%s_%s_var.fits ./residuals0/'%(name,f))
+        #os.popen('cp data/data/sim0_%s_%s_psf.fits ./residuals0/'%(name,f))
+        #original = fits.open('data/data/sim0_%s_%s_sci.fits'%(name,f))
+        original = fits.open('../rangavar/13tousand/3quarry/data/%s_%s_sci.fits'%(name,f))
+        step0[count].data = original[0].data - step0[count].data
+        step0[count].writeto('./lensResAtSource/sim0_%s_%s_sci.fits'%(name,f))
+        source[count+5].writeto('./lensResAtSource/sources/source_%s_%s.fits'%(name,f))
+        permSource = source[count+5].data
+
+        iRange,jRange = numpy.shape(permSource)
+
+        iMax = 0.
+        for i in range(iRange):
+            for j in range(jRange):
+                if permSource[i][j] >= iMax:
+                    iMax = permSource[i][j]
+
+        m = float(mag['%s'%f])
+        #iRe = numpy.exp(-1.) / ( (10.**(m-27.))**(1./2.5) * n * math.gamma(2.*n) * 2. * numpy.pi * re**2. )
+
+        #this definition of Re relies on a non-lensed source, buthere source sizes are different, making it illogical to use the Re for achieving iRe. We rather use the sky variance files instead
+        #iRe = iMax * numpy.exp( - ( (iMax * 2. * numpy.pi * (re**2.) * n * math.gamma(2.*n) )/( 10.**((27.-m)/2.5) ) )**(1./(2.*n)) )
+        #print(iRe)
+
+        #using sky variance to get a threshold pixel value for defining the source borders
+        #and we want the source region from the g-band since in the other bands, it might never go above sky background 
+        if f == 'g':
+            var = fits.open('%s/data/data/sim0_%s_%s_var.fits' %(directory,name,f))[0].data.copy()
+
+            #calculating the mimimum of the variance file 
+            xRange,yRange = numpy.shape(var)
+            minimum = 1000.0
+            for x_pixel in range(xRange):
+                for y_pixel in range(yRange):
+                    if var[x_pixel][y_pixel] < minimum:
+                        minimum = var[x_pixel][y_pixel]
+
+            sigmaSky = minimum**0.5
+            cutoff = 3.*sigmaSky
+            iRe = cutoff
+
+        #bn = 2.*n - 1./3. + 4./(405.*n) + 46./(25515.*(n**2.)) + 131./(1148175.*(n**3.)) - 2194697./(30690717750.*(m**4.))
+        #iRe = ( (10.**((27.-m)/2.5)) * (bn**(2.*n)) )/(numpy.exp(bn)*2.*numpy.pi*(re**2.)*n*math.gamma(2.*n))
+        #print(iRe)
+
+        resAtSource = numpy.zeros((iRange,jRange))
+        sourceSurfaceB = 0
+        resSurfaceB = 0
+        for i in range(iRange):
+            for j in range(jRange):
+                if permSource[i][j] >= iRe:
+                    resAtSource[i][j] = step0[count].data[i][j]
+                    sourceSurfaceB = sourceSurfaceB + permSource[i][j]
+                    resSurfaceB = resSurfaceB + resAtSource[i][j]
+
+        step0[count].data = resAtSource
+        step0[count].writeto('./lensResAtSource/res/res_%s_%s_sci.fits'%(name,f))
+
+        print(name,resSurfaceB,sourceSurfaceB)
+
+        if sourceSurfaceB == 0.:
+            newlines.append('%s %s %s %s %s %s\n' %(name,f,resSurfaceB,sourceSurfaceB,0.0,iRe) )
+        else:
+            newlines.append('%s %s %s %s %s %s\n' %(name,f,resSurfaceB,sourceSurfaceB,resSurfaceB/sourceSurfaceB,iRe) )
+
+        if sourceSurfaceB == 0.:
+            ratio['%s.%s'%(name,f)] = 0.
+        else:
+            ratio['%s.%s'%(name,f)] = resSurfaceB/sourceSurfaceB
+
+        count = count + 1
+
+f = open('lensResAtSource/resRatio.txt','w')
+f.writelines(newlines)
+f.close()
+
+#plotters
+gRatio = []
+iRatio = []
+for name in mags.keys():
+    gRatio.append(float(ratio['%s.%s'%(name,'g')]))
+    iRatio.append(float(ratio['%s.%s'%(name,'i')]))
+
+errorDependency = []
+for i in range(len(gRatio)):
+    x = numpy.log10( (1.+iRatio[i])/(1.+gRatio[i]) )
+    errorDependency.append(x)
+
+cError = []
+for name in mags.keys():
+    cError.append(-real_data[name][-2])
+
+#fit line
+fitED = errorDependency
+fitError = cError
+p = numpy.polyfit(fitED, fitError, 1)
+fittedLine = numpy.poly1d(p)
+x = numpy.arange( min(fitED)-2, max(fitED)+2, 0.1)
+y = fittedLine(x)
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size = '22',weight='bold')
+plt.rcParams["font.family"]='serif'
+plt.rcParams["font.serif"][0] = 'times'
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['ytick.right'] = plt.rcParams['ytick.labelright'] = False
+plt.rcParams['ytick.left'] = plt.rcParams['ytick.labelleft'] = True
+plt.rcParams['xtick.minor.visible'] = True
+plt.rcParams['ytick.minor.visible'] = True
+plt.rcParams['xtick.major.pad'] = 8
+plt.figure(figsize=(10, 8))
+plt.plot(x, y, c='rosybrown', linestyle='--', linewidth = 5, label='best-fit line',zorder=2,alpha=0.8)
+plt.scatter(errorDependency,cError,c='slategrey',s=75,marker='o',label='simulated mock-images',zorder=10,alpha=0.7)
+plt.xlabel('$\omega $',fontsize=24)
+plt.ylabel('$g$-$i$ error ($\delta C$)',fontsize=24)
+plt.xlim(-0.077,0.077)
+plt.ylim(-0.15,0.15)
+plt.axvline(0,c='k',zorder=1,linewidth=1,alpha=0.4)
+plt.axhline(0,c='k',zorder=1,linewidth=1,alpha=0.4)
+plt.legend(fontsize='20')
+plt.savefig('paper_plots/residuals.png',format='png',dpi=400)
 plt.close()
